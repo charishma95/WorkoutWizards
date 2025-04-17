e3402034315ddff8b8d4d8d9b797e4aa68432674
charishma95 <Charishma.Bandari@unh.edu>
2025-04-16 18:21:55 -0700

    Modified App.js to include routing and update component imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
+import Dashboard from './pages/Dashboard';
+import WorkoutLog from './pages/WorkoutLog';
+import Goals from './pages/Goals';
+import NutritionLog from './pages/NutritionLog';
+import WelcomePage from './pages/WelcomePage';  // Import the WelcomePage
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* Navigation can be added here later */}
+        <Routes>
+          <Route path="/" element={<WelcomePage />} /> {/* Set WelcomePage as the default route */}
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/workout-log" element={<WorkoutLog />} />
+          <Route path="/goals" element={<Goals />} />
+          <Route path="/nutrition-log" element={<NutritionLog />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
